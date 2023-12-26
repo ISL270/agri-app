754d81fbbccea5f405bdb2caf8dedb2bb4b9805e
Eslam <eslam@zencode.io>
2023-12-26 03:46:42 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.agri
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:agri/app/app.dart';
+import 'package:agri/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
